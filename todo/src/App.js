08406cb482fa6adf2eee7297d86a40453d85266f
Hamida Sh <sharifovahamida@gmail.com>
2021-01-30 21:42:28 -0800

    Basic  React app

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,38 +1,23 @@
-import React, {useReducer} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {
-  initialState,
-  reducer,
-  Add_Task,
-  Complete_Task,
-  Clear_Task,
-} from "./reducers/taskReducer";
-
-import ToDoForm from './components/ToDoForm';
-import ToDoList from './components/ToDoList';
-
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const newTask = (task) => {
-    dispatch({type: Add_Task, payload: task})
-  }
-
-  const completeTask = (taskId) => {
-    dispatch({type: Complete_Task, payload: taskId})
-  }
-
-  const clearTask = (e) => {
-    e.preventDefault();
-    dispatch({type: Clear_Task})
-  }
-
   return (
     <div className="App">
-      <ToDoForm newTask = {newTask} clearTask = {clearTask} />
-      <ToDoList state ={state} completeTask = {completeTask}/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
